088ccf685fc01292b7c9fbfd4026be45b8c26c4b
naveenkumarmv <38392190+naveenkumarmv@users.noreply.github.com>
2023-04-29 09:16:29 +0530

    add ProductType

---
+++ src/main/java/patternmatching/example/BinaryTernary.java
@@ -0,0 +1,12 @@
+package patternmatching.example;
+
+public class BinaryTernary {
+
+    private Boolean bool;
+    private Ternary ternary;
+
+    public BinaryTernary(Boolean bool, Ternary ternary) {
+        this.bool = bool;
+        this.ternary = ternary;
+    }
+}
